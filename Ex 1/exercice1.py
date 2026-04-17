fdda0912089959b725af9d899bf295cea34d5b9d
Berlal <berlaldemb@gmail.com>
2026-04-17 09:37:07 +0200

    fix ex1

--- Ex 1/exercice1.py
+++ Ex 1/exercice1.py
@@ -8,23 +8,23 @@ ENSEMBLE = [
     {
         "title": "bloc 1",
         "lines": [
-            {"text": "le code propre facilite la maintenance", "show": True}
+            {"text": "Le code propre facilite la maintenance", "show": True}
         ]
     },
     {
         "title": "bloc 2",
         "lines": [
-            {"text": "tester souvent evite beaucoup d erreurs", "show": True},
-            {"text": "cette phrase ne doit pas s afficher", "show": False}
+            {"text": "Tester souvent evite beaucoup d erreurs", "show": True},
+            {"text": "Cette phrase ne doit pas s afficher", "show": False}
         ]
     },
     {
         "title": "bloc 3",
         "lines": [
-            {"text": "cette phrase ne doit pas s afficher", "show": False},
-            {"text": "un bon code doit rester simple et clair", "show": True},
-            {"text": "la simplicite ameliore la qualite du code", "show": True},
-            {"text": "refactoriser ameliore la compréhension", "show": True},
+            {"text": "Cette phrase ne doit pas s afficher", "show": False},
+            {"text": "Un bon code doit rester simple et clair", "show": True},
+            {"text": "La simplicite ameliore la qualite du code", "show": True},
+            {"text": "Refactoriser ameliore la compréhension", "show": True},
         ]
     }
 ]
